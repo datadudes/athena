74707306c62ed5a0f328a23899bd66f3103af46d
Daan Debie <d.debie@thenewmotion.com>
2015-01-26 23:19:13 +0100

    version bump and first PyPI release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='athena',
-    version='0.1.0',
+    version='0.5.0',
     url='https://github.com/datadudes/athena',
     license='MIT',
     author='Daan Debie',
